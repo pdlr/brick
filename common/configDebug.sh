5246fd8438381b74da3bb9001876c490e0c9157f
David LaRose <dlr@cs.cmu.edu>
2011-04-23 19:17:08 -0400

    - Made config*.sh set LDFLAGS. - Changed name in configure.ac to avoid dashes in output filenames.

--- common/configDebug.sh
+++ common/configDebug.sh
@@ -2,6 +2,7 @@ export INSTALLDIR="/var/tmp"
 export COMMONFLAGS="-Wall -Wextra -Wunsafe-loop-optimizations -Wshadow -g"
 
 export CPPFLAGS="-I$INSTALLDIR/include"
+export LDFLAGS="-L$INSTALLDIR/lib"
 export CFLAGS="$COMMONFLAGS"
 export CXXFLAGS="$COMMONFLAGS"
 
--- common/configFast.sh
+++ common/configFast.sh
@@ -2,6 +2,7 @@ export INSTALLDIR="/var/tmp"
 export COMMONFLAGS="-Wall -Wextra -Wunsafe-loop-optimizations -Wshadow -O2"
 
 export CPPFLAGS="-I$INSTALLDIR"
+export LDFLAGS="-L$INSTALLDIR/lib"
 export CFLAGS="$COMMONFLAGS"
 export CXXFLAGS="$COMMONFLAGS"
 

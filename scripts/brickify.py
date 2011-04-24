3f5dfdd955f2a73d38fcda3899181f7518e6f79f
David LaRose <dlr@cs.cmu.edu>
2011-04-23 23:11:07 -0400

    - Added brickify rules for dlrPortability and dlrTest.

--- scripts/brickify.py
+++ scripts/brickify.py
@@ -54,6 +54,8 @@ if __name__ == '__main__':
   os.system("sed "
             + r"-e 's/dlrCommon\/\(\S\)\(\S*\.\S*\)/brick\/common\/\l\1\2/g' "
             + r"-e 's/dlrPortability\/\(\S\)\(\S*\.\S*\)/brick\/portability\/\l\1\2/g' "
+            + r"-e 's/dlrTest\/\(\S\)\(\S*\.\S*\)/brick\/test\/\l\1\2/g' "
+            + r"-e 's/dlrUtilities\/\(\S\)\(\S*\.\S*\)/brick\/utilities\/\l\1\2/g' "
             + r"-e 's/dlrOptimization\/\(\S\)\(\S*\.\S*\)/brick\/optimization\/\l\1\2/g' "
             + r"-e 's/\(brick\/\S*\/\S*\.\)h$/\1hh/' "
             + r"-e 's/\(brick\/\S*\/\S*\.\)h\>/\1hh/' "

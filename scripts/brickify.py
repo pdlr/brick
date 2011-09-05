9f8ee628c32115370bfa550092018eda692c6092
David LaRose <dlr@carnegierobotics.com>
2011-09-05 18:46:18 -0400

    - Added dlrLinearAlgebra to brickify script.

--- scripts/brickify.py
+++ scripts/brickify.py
@@ -62,6 +62,7 @@ if __name__ == '__main__':
             + r"-e 's/dlrPortability\/\(\S\)\(\S*\.\S*\)/brick\/portability\/\l\1\2/g' "
             + r"-e 's/dlrTest\/\(\S\)\(\S*\.\S*\)/brick\/test\/\l\1\2/g' "
             + r"-e 's/dlrNumeric\/\(\S\)\(\S*\.\S*\)/brick\/numeric\/\l\1\2/g' "
+            + r"-e 's/dlrLinearAlgebra\/\(\S\)\(\S*\.\S*\)/brick\/linearAlgebra\/\l\1\2/g' "
             + r"-e 's/dlrUtilities\/\(\S\)\(\S*\.\S*\)/brick\/utilities\/\l\1\2/g' "
             + r"-e 's/dlrOptimization\/\(\S\)\(\S*\.\S*\)/brick\/optimization\/\l\1\2/g' "
             + r"-e 's/\(brick\/\S*\/\S*\.\)h$/\1hh/' "

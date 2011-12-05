4d57f130228762367f58b39f9fcb3967ff95becf
David LaRose <dlr@carnegierobotics.com>
2011-12-04 22:35:38 -0500

    - Fixed broken include path in configFast.sh files.

--- linearAlgebra/configFast.sh
+++ linearAlgebra/configFast.sh
@@ -1,7 +1,7 @@
 export INSTALLDIR="/var/tmp"
 export COMMONFLAGS="-Wall -Wextra -Wunsafe-loop-optimizations -Wshadow -O2"
 
-export CPPFLAGS="-I$INSTALLDIR"
+export CPPFLAGS="-I$INSTALLDIR/include"
 export LDFLAGS="-L$INSTALLDIR/lib"
 export CFLAGS="$COMMONFLAGS"
 export CXXFLAGS="$COMMONFLAGS"

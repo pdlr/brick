6a5e435f2bfbe1e6fd1438941b9d7ae5ab70485d
David LaRose <dlr@cs.cmu.edu>
2012-04-21 02:30:32 -0400

    Added sobel edge operator by porting from dlrComputerVision.

---
+++ computerVision/brick/computerVision/sobel.hh
@@ -0,0 +1,87 @@
+/**
+***************************************************************************
+* @file brick/computerVision/sobel.hh
+*
+* Header file declaring routines to compute sobel edge images.
+*
+* Copyright (C) 2006,2012 David LaRose, dlr@cs.cmu.edu
+* See accompanying file, LICENSE.TXT, for details.
+*
+***************************************************************************
+*/
+
+#ifndef BRICK_COMPUTERVISION_SOBEL_HH
+#define BRICK_COMPUTERVISION_SOBEL_HH
+
+#include <brick/computerVision/image.hh>
+
+namespace brick {
+
+  namespace computerVision {
+    
+
+    /** 
+     * This function applies the sobel edge operator in the X
+     * direction.  Specifically, convolves the image with the 3x3 kernel
+     *
+     * @code
+     *   [[-1, 0, 1],
+     *    [-2, 0, 2],
+     *    [-1, 0, 1]]
+     * @endcode
+     *
+     * and then optionally rescales the result to be proportional to
+     * the image gradient with scale factor 1.0.
+     *
+     * @param inputImage This argument is the image to be convolved.
+     * 
+     * @param normalizeResult This argument specifies whether or not
+     * to divide the resulting pixel values by 8.  Setting this
+     * argument to true will result in lost precision on integer
+     * types.  This feature is currently not implemented, so please
+     * leave normalizeResult at its default value of false.
+     * 
+     * @return The return value is the result of the convolution.
+     */
+    template <ImageFormat FORMAT>
+    Image<FORMAT>
+    applySobelX(const Image<FORMAT>& inputImage, bool normalizeResult=false);
+
+
+    /** 
+     * This function applies the sobel edge operator in the Y
+     * direction.  Specifically, convolves the image with the 3x3 kernel
+     *
+     * @code
+     *   [[-1, -2, -1],
+     *    [ 0,  0,  0],
+     *    [ 1,  2,  1]]
+     * @endcode
+     *
+     * and then optionally rescales the result to be proportional to
+     * the image gradient with scale factor 1.0.
+     *
+     * @param inputImage This argument is the image to be convolved.
+     * 
+     * @param normalizeResult This argument specifies whether or not
+     * to divide the resulting pixel values by 8.  Setting this
+     * argument to true will result in lost precision on integer
+     * types.  This feature is currently not implemented, so please
+     * leave normalizeResult at its default value of false.
+     * 
+     * @return The return value is the result of the convolution.
+     */
+    template <ImageFormat FORMAT>
+    Image<FORMAT>
+    applySobelY(const Image<FORMAT>& inputImage, bool normalizeResult=false);
+
+  } // namespace computerVision
+
+} // namespace brick
+
+
+// Include file containing definitions of inline and template
+// functions.
+#include <brick/computerVision/sobel_impl.hh>
+
+#endif /* #ifndef BRICK_COMPUTERVISION_KERNEL_HH */
---
+++ computerVision/brick/computerVision/test/sobelTest.cc
@@ -0,0 +1,132 @@
+/**
+***************************************************************************
+* @file brick/computerVision/test/sobelTest.cc
+*
+* Source file defining tests for routines defined in
+* brick/computerVision/sobel.hh.
+*
+* Copyright (C) 2006,2012 David LaRose, dlr@cs.cmu.edu
+* See accompanying file, LICENSE.TXT, for details.
+*
+***************************************************************************
+**/
+
+#include <brick/computerVision/test/testImages.hh>
+#include <brick/computerVision/sobel.hh>
+#include <brick/test/testFixture.hh>
+
+namespace brick {
+
+  namespace computerVision {
+    
+    class SobelTest
+      : public brick::test::TestFixture<SobelTest> {
+
+    public:
+
+      SobelTest();
+      ~SobelTest() {}
+
+      void setUp(const std::string& /* testName */) {}
+      void tearDown(const std::string& /* testName */) {}
+
+      // Tests.
+      void testSobelX();
+      void testSobelY();
+
+    private:
+
+    }; // class SobelTest
+
+
+    /* ============== Member Function Definititions ============== */
+
+    SobelTest::
+    SobelTest()
+      : TestFixture<SobelTest>("SobelTest")
+    {
+      BRICK_TEST_REGISTER_MEMBER(testSobelX);
+      BRICK_TEST_REGISTER_MEMBER(testSobelY);
+    }
+
+
+    void
+    SobelTest::
+    testSobelX()
+    {
+      brick::numeric::Array2D<brick::common::Int32> inputArray(
+        "[[ 0,  1,  0, -2],"
+        " [ 7,  5,  2,  3],"
+        " [10,  9,  8,  7],"
+        " [ 3,  2,  3,  4],"
+        " [ 2,  2,  5,  9]]");
+
+      brick::numeric::Array2D<brick::common::Int32> referenceArray(
+        "[[  8,   0, -12, -16],"
+        " [ -8, -12,  -9,  -2],"
+        " [-10,  -9,  -4,   0],"
+        " [ -6,   1,   9,  10],"
+        " [  0,  12,  28,  32]]");
+    
+      Image<GRAY_SIGNED32> inputImage = inputArray;
+      Image<GRAY_SIGNED32> resultImage = applySobelX(inputImage);
+
+      BRICK_TEST_ASSERT(resultImage.rows() == referenceArray.rows());
+      BRICK_TEST_ASSERT(resultImage.columns() == referenceArray.columns());
+      for(size_t index0 = 0; index0 < resultImage.size(); ++index0) {
+        BRICK_TEST_ASSERT(resultImage[index0] == referenceArray[index0]);
+      }
+    }
+
+
+    void
+    SobelTest::
+    testSobelY()
+    {
+      brick::numeric::Array2D<brick::common::Int32> inputArray(
+        "[[ 0,  1,  0, -2],"
+        " [ 7,  5,  2,  3],"
+        " [10,  9,  8,  7],"
+        " [ 3,  2,  3,  4],"
+        " [ 2,  2,  5,  9]]");
+
+      brick::numeric::Array2D<brick::common::Int32> referenceArray(
+        "[[ 56, 34,  26,  40],"
+        " [ 40, 34,  33,  36],"
+        " [-16, -9,   0,   4],"
+        " [-32,-25, -11,   8],"
+        " [ -8,  2,  18,  40]]");
+    
+      Image<GRAY_SIGNED32> inputImage = inputArray;
+      Image<GRAY_SIGNED32> resultImage = applySobelY(inputImage);
+
+      BRICK_TEST_ASSERT(resultImage.rows() == referenceArray.rows());
+      BRICK_TEST_ASSERT(resultImage.columns() == referenceArray.columns());
+      for(size_t index0 = 0; index0 < resultImage.size(); ++index0) {
+        BRICK_TEST_ASSERT(resultImage[index0] == referenceArray[index0]);
+      }
+    }
+
+  } // namespace computerVision
+  
+} // namespace brick
+
+
+#if 0
+
+int main(int argc, char** argv)
+{
+  brick::computerVision::SobelTest currentTest;
+  bool result = currentTest.run();
+  return (result ? 0 : 1);
+}
+
+#else
+
+namespace {
+
+  brick::computerVision::SobelTest currentTest;
+
+}
+
+#endif

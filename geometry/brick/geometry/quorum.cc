bce839a93b17ffb7aad680ce0cd93022cd8cb165
David LaRose <dlr@carnegierobotics.com>
2013-05-29 15:25:58 -0400

    - Added a .cc file to brickGeometry to avoid issues with recent compilers.

---
+++ geometry/brick/geometry/quorum.cc
@@ -0,0 +1,23 @@
+/**
+***************************************************************************
+* @file brick/geometry/quorum.cc
+*
+* Dummy source file so that recent compilers won't complain about
+* building a library with no objects.
+*
+* Copyright (C) 2013 David LaRose, dlr@cs.cmu.edu
+* See accompanying file, LICENSE.TXT, for details.
+*
+***************************************************************************
+**/
+
+namespace brick {
+
+  namespace geometry {
+
+    /// Unused. 
+    unsigned int representative = 0;
+    
+  } // namespace geometry
+  
+} // namespace brick

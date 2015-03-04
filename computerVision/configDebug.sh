39fcbe5df1778ad5d8927f4c52d895289d66323d
David LaRose <dlr@carnegierobotics.com>
2015-03-03 19:30:30 -0500

    Added array bounds checking to debug build.

--- computerVision/configDebug.sh
+++ computerVision/configDebug.sh
@@ -1,6 +1,6 @@
 export INSTALLDIR="/var/tmp"
 # export COMMONFLAGS="-Wall -Werror -Wextra -Wunsafe-loop-optimizations -Wshadow -g"
-export COMMONFLAGS="-Wall -Werror -Wextra -Wshadow -g"
+export COMMONFLAGS="-Wall -Werror -Wextra -Wshadow -g -DBRICK_NUMERIC_CHECKBOUNDS=1"
 
 export CPPFLAGS="-I$INSTALLDIR/include"
 export LDFLAGS="-L$INSTALLDIR/lib"

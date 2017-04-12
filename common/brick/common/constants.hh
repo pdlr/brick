6ef492b53aa826530df000ac8338d432d509ac33
David LaRose <dlr@carnegierobotics.com>
2017-04-12 19:08:32 -0400

    Made constants be constexpr, if supported by the compiler.

--- common/brick/common/constants.hh
+++ common/brick/common/constants.hh
@@ -15,6 +15,12 @@
 
 #include <brick/common/types.hh>
 
+#if __cplusplus <= 199711L
+#define BRICK_CONSTEXPR const
+#else
+#define BRICK_CONSTEXPR constexpr
+#endif
+
 namespace brick {
 
   namespace common {
@@ -22,27 +28,28 @@ namespace brick {
     namespace constants {
 
       // Basic physical constants.
-      const brick::common::Float64 e =
+      BRICK_CONSTEXPR brick::common::Float64 e =
         2.71828182845904523536028747135266249775724709369995;
-      const brick::common::Float64 pi =
+      BRICK_CONSTEXPR brick::common::Float64 pi =
         3.14159265358979323846264338327950288419716939937510;
-      const brick::common::Float64 rootTwo =
+      BRICK_CONSTEXPR brick::common::Float64 rootTwo =
         1.41421356237309504880168872420969807856967187537694;
 
       // Empirical constants.
-      const brick::common::Float64 avogadro = 6.02214179E23;
+      BRICK_CONSTEXPR brick::common::Float64 avogadro = 6.02214179E23;
 
       // Constants that derive from the basic ones.
-      const brick::common::Float64 degreesPerRadian = 180.0 / pi;
-      const brick::common::Float64 piOverFour = pi / 4.0;
-      const brick::common::Float64 piOverTwo = pi / 2.0;
-      const brick::common::Float64 radiansPerDegree = pi / 180.0;
-      const brick::common::Float64 rootTwoOverTwo = rootTwo / 2.0;
-      const brick::common::Float64 rootOverTwo = rootTwoOverTwo;
-      const brick::common::Float64 threePiOverFour = piOverTwo + piOverFour;
-      const brick::common::Float64 threePiOverTwo = pi + piOverTwo;
-      const brick::common::Float64 twoPi = 2.0 * pi;
 
+      BRICK_CONSTEXPR brick::common::Float64 degreesPerRadian = 180.0 / pi;
+      BRICK_CONSTEXPR brick::common::Float64 piOverFour = pi / 4.0;
+      BRICK_CONSTEXPR brick::common::Float64 piOverTwo = pi / 2.0;
+      BRICK_CONSTEXPR brick::common::Float64 radiansPerDegree = pi / 180.0;
+      BRICK_CONSTEXPR brick::common::Float64 rootTwoOverTwo = rootTwo / 2.0;
+      BRICK_CONSTEXPR brick::common::Float64 rootOverTwo = rootTwoOverTwo;
+      BRICK_CONSTEXPR brick::common::Float64 threePiOverFour = piOverTwo + piOverFour;
+      BRICK_CONSTEXPR brick::common::Float64 threePiOverTwo = pi + piOverTwo;
+      BRICK_CONSTEXPR brick::common::Float64 twoPi = 2.0 * pi;
+      
     } // namespace contants
 
   } // namespace common

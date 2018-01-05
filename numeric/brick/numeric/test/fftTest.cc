08ba44c6ab02c6cb1d7fb346ad01bf238e3327cc
David LaRose <dlr@carnegierobotics.com>
2018-01-05 09:17:11 -0500

    Added another test for computeFFT() and expanded header comments slightly.

--- numeric/brick/numeric/test/fftTest.cc
+++ numeric/brick/numeric/test/fftTest.cc
@@ -35,6 +35,7 @@ namespace brick {
       // Tests.
       void testComputeFFT_radix2();
       void testComputeFFT_result();
+      void testComputeFFT_singleFrequency();
     
     private:
 
@@ -57,6 +58,7 @@ namespace brick {
       // Register all tests.
       BRICK_TEST_REGISTER_MEMBER(testComputeFFT_radix2);
       BRICK_TEST_REGISTER_MEMBER(testComputeFFT_result);
+      BRICK_TEST_REGISTER_MEMBER(testComputeFFT_singleFrequency);
     }
 
 
@@ -142,8 +144,8 @@ namespace brick {
                                  this->m_defaultTolerance));
           }
         } catch(...) {
-          std::cout << "FFTTest::testComputeFFT failure at element " << ii
-                    << ":\n"
+          std::cout << "FFTTest::testComputeFFT_result()\n"
+                    << "  Failure at element " << ii << ":\n"
                     << "  amplitude " << amplitude
                     << " at phase " << phase << " vs. reference: \n"
                     << "  amplitude " << referenceAmplitudes[ii]
@@ -156,7 +158,115 @@ namespace brick {
         }
       }
     }
-  
+
+
+    void
+    FFTTest::
+    testComputeFFT_singleFrequency()
+    {
+      double constexpr onePi = brick::common::constants::pi;
+      double constexpr twoPi = brick::common::constants::twoPi;
+      std::size_t constexpr signalLength = 1024;
+
+      // We'll deal with real-valued signals of a single frequency,
+      // where the frequency ranges from zero to Nyquist.
+      for(std::size_t ii = 0; ii < signalLength / 2; /* ++ii */ ii += 2) {
+        // What's the frequency in radians per sample.
+        double elementAmplitude = 1.0 / signalLength;
+        double frequency = twoPi * double(ii) / signalLength;
+        
+        // Compute a signal that has just this one frequency.  For
+        // efficiency, we might move this declaration outside of the
+        // enclosing for loop, but it's clearer here.
+        Array1D< std::complex<double> > inputSignal(signalLength);
+
+        // For each element of the input sequence.
+        for(std::size_t jj = 0; jj < signalLength; ++jj) {
+          inputSignal[jj] = std::complex<double>(
+            elementAmplitude * brick::common::cosine(jj * frequency),
+            0.0);
+        }
+
+        // std::cout << "\nSignal: " << inputSignal << std::endl;
+      
+        // Now do the FFT.
+        Array1D< std::complex<double> > fft = computeFFT(inputSignal);
+
+        // std::cout << "\nFFT: " << fft << std::endl;
+      
+        // Check that the result is correct.
+        for(std::size_t jj = 0; jj < signalLength; ++jj) {
+          double amplitude = std::abs(fft[jj]);
+          double phase = brick::common::arctangent2(
+            fft[jj].imag(), fft[jj].real());
+          while(phase < -onePi) {phase += twoPi;}
+          while(phase >= onePi) {phase -= twoPi;}
+
+          try {
+            if(jj == ii) {
+
+              if(ii == 0 || ii == (signalLength / 2)) {
+                
+                // Element 0 represents DC component.  When frequency
+                // is 0, all the energy goes into this element, so the
+                // Fourier coefficient should be 1.0.  Similarly,
+                // element signalLength / 2 represents frequency pi
+                // radians per sample, which aliases to itself.  Again
+                // we expect coeffient == 1.
+                BRICK_TEST_ASSERT(
+                  approximatelyEqual(amplitude, 1.0, this->m_defaultTolerance));
+                
+              } else {
+
+                // Energy at all other frequencies gets split between
+                // the Fourier coefficient at index ii and the aliased
+                // frequency (above Nyquist) at (signalLength - ii).
+                // These correspond to frequencies of (2 * pi * ii /
+                // N) radians per sample and (2 * pi * (1 - (ii / N)))
+                // radians per sample, respectively.  We expect both
+                // of these coefficients to be 0.5.
+                BRICK_TEST_ASSERT(
+                  approximatelyEqual(amplitude, 0.5, this->m_defaultTolerance));
+                
+              }
+
+              // In either case, phase should be zero, because we didn't
+              // introduce any phase shift when we constructed the signal.
+              BRICK_TEST_ASSERT(
+                approximatelyEqual(phase, 0.0, this->m_defaultTolerance));
+              
+            } else if (jj == (signalLength - ii)) {
+
+                // This is the (2 * pi * (1 - (ii / N))) radians per
+                // sample component mentioned above.
+                BRICK_TEST_ASSERT(
+                  approximatelyEqual(amplitude, 0.5, this->m_defaultTolerance));
+                BRICK_TEST_ASSERT(
+                  approximatelyEqual(phase, 0.0, this->m_defaultTolerance));
+
+            } else {
+
+              // All other components should have zero amplitude.  Because
+              // amplitude is zero (ish), phase is ill-conditioned, so we
+              // ignore phase.
+              BRICK_TEST_ASSERT(
+                approximatelyEqual(amplitude, 0.0, this->m_defaultTolerance));
+              // BRICK_TEST_ASSERT(
+              //   approximatelyEqual(phase, 0.0, this->m_defaultTolerance));
+              
+            }
+          } catch(...) {
+            std::cout << "FFTTest::testComputeFFT_singleFrequency()\n"
+                      << "  Failure at freqency " << ii << ", element " << jj
+                      << ":\n"
+                      << "  amplitude " << amplitude
+                      << " at phase " << phase << std::endl;
+            throw;
+          }
+        }
+      }
+    }
+    
   } //  namespace numeric
 
 } // namespace brick
